c864902bba12667a862522dc98ab3979ca511490
Abdel-Nasser <77011658+Abdel-Nasser@users.noreply.github.com>
2021-01-16 18:56:38 +0100

    Update 03_stairway.rb

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,1 +1,74 @@
 
+step  = 0
+tour = 0
+
+puts "vous étes à cette marche: |__"
+  print "vous pouvez commencer! rentrer un nombre entre 1 à 6"
+  puts " >"
+
+
+ user = gets.chomp.to_i
+
+
+ loop do
+
+     if   step != 10
+
+ 
+    case user
+
+    
+    when 5..6
+
+        puts "vous avez monter d'une marches!"
+       puts "  |__"
+     puts " __"
+     step +=1
+     user = gets.chomp.to_i
+     print ">"
+
+
+
+        
+    when  1
+       puts "vous reculer d'une marches!"
+       puts   "__"
+     puts " |__"
+     step -=1
+     user = gets.chomp.to_i
+     print ">"
+
+
+    
+    when  2..4
+        puts "rien ne se passe" 
+        user = gets.chomp.to_i
+        print ">"
+
+    
+    
+
+    else 
+  puts "bravo vous pouvez voir la tour eiffel de cette étage!!!! #{step}"
+           puts "woahhhhh"
+puts "          |__"    +        " | "
+puts "         __"     +         " | "
+puts "        __"      +         " | "
+puts "       __"      +          " | "
+puts "      __"       +          "_|_ "
+puts "     __"       +         "|"   "|"
+puts "    __"       +          "|"   "|"
+puts "   __"       +          "|"      "|"
+puts "  __"       +          "|"        "|"
+puts " __"       +          "|"          "|"
+
+       break
+
+    
+    end
+        
+
+  end 
+
+end  
+  
